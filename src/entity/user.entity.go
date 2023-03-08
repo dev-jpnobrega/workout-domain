17a3e929a0c1d13921e39329ab3382c4fda753a5
“devjpnobrega” <joaopaulonobrega@hotmail.com>
2023-03-08 14:32:28 -0300

    add user.entity

---
+++ src/entity/user.entity.go
@@ -0,0 +1,19 @@
+package entity
+
+import (
+	"strings"
+
+	"github.com/google/uuid"
+)
+
+// A User entity
+type User struct {
+	UserId    uuid.UUID `json:"userId" param:"userId"`
+	UserName  string    `json:"userName" form:"userName" query:"userName" validate:"required"`
+	Email     string    `json:"email" form:"email" query:"email" `
+	CompanyID int       `json:"companyId" form:"companyId" query:"companyId"`
+}
+
+func (u *User) IsValid() bool {
+	return (len(strings.TrimSpace(u.UserName)) > 0)
+}
